81a4f54b2fa51d0b01c0eceef33f0bad8816cd27
paulinedusfourcastan <113920378+paulinedusfourcastan@users.noreply.github.com>
2023-03-13 16:57:37 +0100

    petites modif

--- R/jeu_tout_seul.R
+++ R/jeu_tout_seul.R
@@ -7,23 +7,21 @@ source("case_revelation.R")
 # Fonction qui joue au démineur seule 
 
 # nv = niveau de difficulté
-# nb_part = nombre de case que l'on veut que l'ordi revele 
+# nb_part = nombre de case que l'on veut que l'ordi revele
+# taille = taille de la grille 
 
-jeu_seul <- function (nv, nb_part){
-  N <- 15
-  
+jeu_seul <- function (nv, nb_part, taille){
+
   if(nv==1){nb_bomb<-50}
   if(nv==2){nb_bomb<-70}
   if(nv==3){nb_bomb<-100}
   
-  gg_d <- creation_grille(nb_bomb)
+  gg_d <- creation_grille(nb_bomb, taille)
   
-  A <- crea_vide(nv)
-  
-  
+  A <- crea_vide(nb_bomb, taille)
   
   for (i in 1:nb_part){
-    n <- sample(1:225, size = 1)
+    n <- sample(1:taille^2, size = 1)
     print(n)
     if (gg_d[n]!=-6){
       A <- case_revel(n, A, gg_d)
@@ -40,3 +38,5 @@ jeu_seul <- function (nv, nb_part){
   }
   
 }
+
+
