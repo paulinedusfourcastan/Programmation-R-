5a1f3ffa9727c9831449c9b971183c903da0eb06
paulinedusfourcastan <113920378+paulinedusfourcastan@users.noreply.github.com>
2023-03-18 08:19:26 +0100

    ajout de commentaires

--- R/case_revelation.R
+++ R/case_revelation.R
@@ -3,6 +3,7 @@ library(emojifont)
 library(dplyr)
 
 # Fonction qui révèle la valeur d'une case
+
 # case_id = numéro de la case que l'on veut ouvrir
 # grille1 = grille vide créée avec crea_vide()
 # grille2 = grille de démineur créée avec creation_grille()
@@ -11,6 +12,7 @@ case_revel <- function(case_id, grille1, grille2){
   t <- nrow(grille1)
   
   # dans le cas des bombes 
+  # on affecte un emoji bombe à toutes les cases bombes et on retourne la matrice finale 
   if (grille2[case_id]== -6){
     grille1[case_id] <- emoji("bomb")
     for (i in (1:t^2)){
@@ -23,13 +25,16 @@ case_revel <- function(case_id, grille1, grille2){
   }
   
   # dans le cas voisin = bombe
+  # on revele le nombre de bombes voisines à cette case 
   if (grille2[case_id]!= -6  & grille2[case_id]!=0){
     grille1[case_id] <- grille2[case_id]
     return(grille1)
   }
   
   # dans le cas pas de voisin bombe
+  # il faut reveler la valeur des cases autour 
   if (grille2[case_id]==0){
+    
     # coin haut gauche
     if (case_id==1){
       grille1[case_id] <- grille2[case_id]
@@ -107,7 +112,7 @@ case_revel <- function(case_id, grille1, grille2){
       return(grille1)
     }
     
-    
+    # si la case n'est pas un coin ou un bord 
     else{
       grille1[case_id] <- grille2[case_id]
       grille1[case_id + 1] <- grille2[case_id +1]
