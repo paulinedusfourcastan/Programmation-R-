9827db8004a288d3c6a8d443a79c2b0554f0b6fe
paulinedusfourcastan <113920378+paulinedusfourcastan@users.noreply.github.com>
2023-03-19 07:59:58 +0100

    je met tout dans une fonction au lieu de deux

--- R/drapeau_pose.R
+++ R/drapeau_pose.R
@@ -1,20 +1,21 @@
 library(emoji)
 library(emojifont)
 
-# Fonction pour placer un drapeau 
+# Fonction pour placer ou enlever un drapeau 
+# drapeau = numero de la case où on veut poser ou enlever un drapeau 
+
 place_drap <- function(drapeau, grille){
   t <- nrow(grille)
   drap <- matrix(data =paste0(emoji("triangular_flag_on_post"),1:(t^2)), nrow = t, ncol = t)
-  grille[drapeau] <- drap[drapeau]
-  return(grille)
+  gg <- matrix(data =paste0(emoji("sunflower"),1:(t^2)), nrow = t, ncol = t)
+  
+  if (grille[drapeau]==drap[drapeau]){
+    grille[drapeau] <- gg[drapeau]
+    return(grille)
+  }
+  else{
+    grille[drapeau] <- drap[drapeau]
+    return(grille)}
   
 }
 
-
-# Fonction pour enlever un drapeau 
-deplace_drap <- function(drapeau, grille){
-  t <- nrow(grille)
-  gg <- matrix(data =paste0(emoji("sunflower"),1:(t^2)), nrow = t, ncol = t)
-  grille[drapeau] <- gg[drapeau]
-  return(grille)
-}
